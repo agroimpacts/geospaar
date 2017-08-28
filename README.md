e552e78e5a6ccac847f8e3243607be286c971e6b
ldemaz <3401881+ldemaz@users.noreply.github.com>
2017-08-28 06:50:15 -0400

    Contructing readme, updated help.html, syllabus

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # geospaR
 The main repository for Clark University's Geospatial Analysis with R course (GEOG 246/346). All course materials will be presented here. 
+
+If you are here and reading this, then you already have access to the private GitHub repository on which this is hosted.  However, we want to install this as an R package. To do that, please follow the instructions here
+
+# Installation
+
+1. Get the `devtools` package:
+
+```r
+install.packages("devtools")
+library(devtools)
+```
+
+2. Get a personal access token for GitHub
+
+We'll cheat a little bit here and direct you to the directions in the [help file](vignettes/help.html) vignette, before it is installed. That shows you how to get an access token. 
+
--- vignettes/syllabus.R
+++ vignettes/syllabus.R
@@ -1,3 +1,25 @@
 ## ----setup, echo=FALSE, include=FALSE------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
+library(knitr)
+
+## ---- echo = FALSE-------------------------------------------------------
+mat <- rbind(c("Practical assignments (n=6)", "30% of grade", "20% of grade"), 
+            c("Oral presentation of semester project", "20% of grade", 
+              "30% of grade"),
+            c("Final report on semester project", "40% of grade", 
+              "40% of grade"))
+colnames(mat) <- c("Component", "GEOG246", "GEOG346")
+kable(mat)                 
+
+## ---- echo = FALSE-------------------------------------------------------
+gr <- c(37, 12, 40, 65, 10, 16)
+ug <- c(37, 14, 50, 59, 8, 12)
+DF <- cbind.data.frame(c("Class meetings", "Readings", "Practicals", 
+                         "Semester projects-analysis", 
+                         "Semester projects-presentation",
+                         "Semester projects-final report", "Total hours"), 
+                       c(gr, sum(gr)), c(ug, sum(ug)))
+colnames(DF) <- c("", "GEOG246 hours", "GEOG346 hours")
+kable(DF)                 
+
 
