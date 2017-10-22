4baaee737f3abf15e352d1666014ae2afffadd4b
ldemaz <3401881+ldemaz@users.noreply.github.com>
2017-10-22 16:33:01 +0200

    Added plot_noaxes function to package. Raster section 2 vignette drafted through additional processing sub-section

---
+++ R/plot_noaxes.R
@@ -0,0 +1,23 @@
+#' Plot rasters without axes by default
+#' @description This is a small variant on raster::plot that provides default
+#' margin parameters and sets axes and box arguments to false
+#' @param x An object of class Raster*
+#' @param axes Defaults to FALSE, meaning axes with coordinates are not plotted
+#' @param box Defaults to FALSE, meaning box drawn is not drawn around plot
+#' @param mar Sets the default margins for a single plot to c(0, 0, 1, 4)
+#' @param ... Graphical parameters. Any argument that can be passed to
+#' image.plot and to plot, such as main='title', ylab='latitude'
+#' @return A plotted raster
+#' @examples
+#' data(chirps)
+#' plot_noaxes(x = chirps[[1]])
+#' plot_noaxes(x = chirps[[1:3]])
+#' @export
+plot_noaxes <- function(x, axes = FALSE, box = FALSE, mar = c(0, 0, 1, 4),
+                        ...) {
+  if(!class(x) %in% c("RasterLayer", "RasterStack", "RasterBrick")) {
+    stop("This function is intended for rasters only", call. = FALSE)
+  }
+  par(mar = mar)
+  plot(x, axes = axes, box = axes, ...)
+}
