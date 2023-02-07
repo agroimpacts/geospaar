c798c4f1a4eefcf76331ac0b4c5a8d49609081c2
mcecil <michaeljcecil@gmail.com>
2023-02-07 14:02:09 -0500

    update "knit_rmd" function

--- data_raw/knit_rmd.R
+++ data_raw/knit_rmd.R
@@ -1,13 +1,24 @@
 
-patterns = c("index.Rmd", "unit1-module3.Rmd")
+patterns = c("unit1-module3.Rmd", "unit1-module2.Rmd" )
 
 rmds <- dir(here::here("vignettes"), pattern = "Rmd", full.names = TRUE)
-
-
 for (pattern in patterns){
+  print(pattern)
   ifile <- rmds[grep(pattern, rmds)]
   ohtml <- here::here("docs",
                       paste0(gsub("\\.Rmd", "", basename(ifile)), ".html"))
   rmarkdown::render(input = ifile, output_file = ohtml)
 }
 
+## in docs folder
+
+patterns = c("index.Rmd" )
+
+rmds <- dir(here::here("docs"), pattern = "Rmd", full.names = TRUE)
+for (pattern in patterns){
+  print(pattern)
+  ifile <- rmds[grep(pattern, rmds)]
+  ohtml <- here::here("docs",
+                      paste0(gsub("\\.Rmd", "", basename(ifile)), ".html"))
+  rmarkdown::render(input = ifile, output_file = ohtml)
+}
